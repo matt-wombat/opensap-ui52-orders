6f8c9aab0a24c735c4309ae0217e4f2e87f055ac
Matthäus Otremba <matthaeus.otremba@procon-it.de>
2021-01-13 13:17:39 +0000

    Week 3 Unit 4 - Exercise: Spicing up Your Scenario with Feature-Rich Controls

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -15,6 +15,26 @@ sap.ui.define([], function () {
 			}
 
 			return parseFloat(sValue).toFixed(2);
+		},
+
+		/**
+		 * Converts the delivery status value from the data into a
+		 * number used for the colorScheme propery of the InfoLabel
+		 * control.
+		 *
+		 * @public
+		 * @param {string} sValue The value to be formatted
+		 * @returns {int} The formatted delivery status value for the
+		 * InfoLabel colorScheme
+		 */
+		deliveryStatus: function(sValue) {
+			switch(sValue) {
+				case "N": return 5; 
+		        case "P": return 1;
+		        case "X": return 3;
+		        case "C": return 7;
+				default: return 7;
+			}
 		}
 	};
 });
