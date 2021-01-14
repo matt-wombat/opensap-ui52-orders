739bbdd816325ea704af5ace4d5ba1ccdb8bc0e1
Matthäus Otremba <matthaeus.otremba@procon-it.de>
2021-01-14 17:08:29 +0000

    Week 4 Unit 2 - Exercise: Improving User Experience with Drag and Drop

---
+++ webapp/control/DeleteButton.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/m/Button"
+], function (Button) {
+	"use strict";
+
+	return Button.extend("opensap.opensap-ui52-orders.control.DeleteButton", {
+
+		metadata : {
+			dnd: {droppable: true}
+		},
+		renderer : {}
+	});
+});
